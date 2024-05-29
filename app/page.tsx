7cdd242f8febefe4291b2edf96811fa92a24d2df
Emiliano Neaves <emilianoneaves@MacBook-Air-de-Emiliano-2.local>
2024-05-28 18:32:41 -0600

    paginas de preference lista

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,31 @@
-export default function Home() {
-	return (
-	  <main className="w-full h-screen flex flex-col">
-		<div className="bg-custom-yellow bg-cover bg-center h-full flex flex-col items-center justify-center">
-		  <div className="flex items-center justify-start w-full overflow-hidden">
-			<img src="/lp-aimax.png" className="w-[750px] h-[480px] rounded-3xl -translate-x-10"/> 
-			<div className="ml-40">
-			  <h1 className="text-white text-7xl font-light italic mb-4">AIMAX</h1>
-			  <button className="bg-custom-orange w-40 font-light italic text-white py-2 px-4 rounded-3xl">
-				START
-			  </button>
-			</div>
-		  </div>
-		</div>
-	  </main>
-	);
-}
+"use client";
 
+import React from 'react';
+import { useRouter } from 'next/navigation';
+
+export default function Home() {
+  const router = useRouter();
+
+  const handleStartClick = () => {
+    router.push('/login'); // Ruta a la página de login
+  };
+
+  return (
+    <main className="w-full h-screen flex flex-col">
+      <div className="bg-custom-yellow bg-cover bg-center h-full flex flex-col items-center justify-center">
+        <div className="flex items-center justify-start w-full overflow-hidden">
+          <img src="/lp-aimax.png" className="w-[750px] h-[480px] rounded-3xl -translate-x-10" /> 
+          <div className="ml-40">
+            <h1 className="text-white text-7xl font-light italic mb-4">AIMAX</h1>
+            <button
+              className="bg-custom-orange w-40 font-light italic text-white py-2 px-4 rounded-3xl"
+              onClick={handleStartClick}
+            >
+              START
+            </button>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -1,50 +1,22 @@
 import React from "react";
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import SignUpButton from "../components/signup-button";
 
-function SignInPage() {
+function SignupPage() {
   return (
-    <div className="flex justify-center items-center min-h-screen bg-custom-yellow">
+    <div className="flex justify-center items-center h-full bg-custom-yellow">
       <Card className="w-[350px] bg-custom-orange rounded-3xl">
         <CardHeader className="flex flex-col items-center">
-          <CardTitle className="text-white text-4xl font-light italic">AIMAX</CardTitle>
+          <CardTitle className="text-white text-4xl font-light italic">
+            AIMAX
+          </CardTitle>
         </CardHeader>
         <CardContent>
-          <form>
-            <div className="grid w-full items-center gap-4">
-              <div className="flex flex-col space-y-1.5">
-                <Label htmlFor="username" className="text-white">
-                  Username
-                </Label>
-                <Input id="username" placeholder="Username" className="bg-gray-200" />
-              </div>
-              <div className="flex flex-col space-y-1.5">
-                <Label htmlFor="password" className="text-white">
-                  Password
-                </Label>
-                <Input id="password" placeholder="Password" className="bg-gray-200" />
-              </div>
-            </div>
-          </form>
+          <SignUpButton />
         </CardContent>
-        <CardFooter className="flex flex-col items-center">
-          <Button className="bg-custom-blue text-white w-28  rounded-3xl">Sign Up</Button>
-          <div className="flex justify-center gap-4 mt-4">
-          <img src="/github-icon.png" alt="Google" className="w-12 h-12 rounded-full"/>
-          </div>
-        </CardFooter>
       </Card>
     </div>
   );
 }
 
-export default SignInPage;
+export default SignupPage;
